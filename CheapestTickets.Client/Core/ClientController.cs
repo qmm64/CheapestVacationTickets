41468dc35720f45fe9cc2dc8afcc9458e61395cc
qmm64 <10gmagma01@gmail.com>
2026-08-14 15:38:47 +0700

    Написан ReadMe файл. ClientController переведён из режима тестирования в рабочий режим

--- CheapestTickets.Client/Core/ClientController.cs
+++ CheapestTickets.Client/Core/ClientController.cs
@@ -1,4 +1,5 @@
-﻿using CheapestTickets.Shared.Models;
+﻿using CheapestTickets.Client.Services;
+using CheapestTickets.Shared.Models;
 
 namespace CheapestTickets.Client.Core
 {
@@ -8,17 +9,17 @@ namespace CheapestTickets.Client.Core
         {
             var ui = new UserInterface();
 
-            var routes = new List<FlightRoute>
-            {
-                new FlightRoute("OVB", "SEL", DateOnly.FromDateTime(DateTime.Now)),
-                new FlightRoute("SEL", "OSA", DateOnly.FromDateTime(DateTime.Now.AddDays(3))),
-                new FlightRoute("TYO", "BJS", DateOnly.FromDateTime(DateTime.Now.AddDays(13))),
-                new FlightRoute("BJS", "OVB", DateOnly.FromDateTime(DateTime.Now.AddDays(16)))
-            };
+            //var routes = new List<FlightRoute>
+            //{
+            //    new FlightRoute("OVB", "SEL", DateOnly.FromDateTime(DateTime.Now)),
+            //    new FlightRoute("SEL", "OSA", DateOnly.FromDateTime(DateTime.Now.AddDays(3))),
+            //    new FlightRoute("TYO", "BJS", DateOnly.FromDateTime(DateTime.Now.AddDays(13))),
+            //    new FlightRoute("BJS", "OVB", DateOnly.FromDateTime(DateTime.Now.AddDays(16)))
+            //};
 
             while (true) 
             {
-                //var routes = FlightBuilder.AddFlights();
+                var routes = FlightBuilder.AddFlights();
                 int days = RequestHandler.GetCount("Введите количество дней");
                 var client = new Services.Client();
                 var request = new FlightRequest(routes, days);
